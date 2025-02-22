f8329964c880cd14a8ecddbcf90428056f2479aa
Bola <Hsh.bola28@gmail.com>
2025-02-22 18:20:00 +0500

    22.02.25

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,6 @@
 "use strict";
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Error!');
-}
-
-const num = 50;
-if (num < 49) {
-    console.log('Error!');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-    console.log('Ok!');
-}
-
-const num = 50;
-(num === 50) ? console.log('Ok!') : console.log('Error!');
-
-const num = 50;
-switch (num) {
-    case 49:
-        console.log('Error!');
-        break;
-    case 100:
-        console.log('Error!');
-        break;
-    case 50:
-            console.log('Верно!');
-        break;
-    default:
-            console.log('Не в этот раз');
-        break;
+const gamburger = true;
+const fries = true;
+if (gamburger && fries) {
+    console.log('Я сыт!');
 }
