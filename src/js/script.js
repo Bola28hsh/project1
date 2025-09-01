263f2a0b4721eeb6d2a234642838237c67d9faa7
Bola <Hsh.bola28@gmail.com>
2025-09-02 03:01:14 +0500

    02/09/25 commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 "use strict";
-const gamburger = true;
-const fries = true;
-if (gamburger && fries) {
-    console.log('Я сыт!');
+for (let i = 0; i < 2; i++) {
+    const a = prompt ( `Один из последних просмотренных фильмов?`, `` );
+    const b = prompt ( `На сколько оцените его?`, `` );
+
+if (a != null && b != null && a != `` && b != `` && a.length < 50) {
+  personalMovieDB.movies[a] = b;
+  console.log( `done` );
+} else {
+   console.log( `error` );
+   i--;
 }
+
+}
+
+console.log(personalMovieDB);
