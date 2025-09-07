3167a3d0c613be67adc035c056e7a4f02c16e69c
Bola <Hsh.bola28@gmail.com>
2025-09-07 17:22:26 +0500

    07.09.25 commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,9 @@
+
 "use strict";
-for (let i = 0; i < 2; i++) {
-    const a = prompt ( `Один из последних просмотренных фильмов?`, `` );
-    const b = prompt ( `На сколько оцените его?`, `` );
+let a = 3;
+const b = 'AbC';
+let c = 3;
+const IsChecked = true,
+      IsClose = true;
 
-if (a != null && b != null && a != `` && b != `` && a.length < 50) {
-  personalMovieDB.movies[a] = b;
-  console.log( `done` );
-} else {
-   console.log( `error` );
-   i--;
-}
-
-}
-
-console.log(personalMovieDB);
+      console.log(IsChecked && ! IsClose);
